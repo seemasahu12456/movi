007fc8c239448f3663e2bfb188e5b03a44c4414a
seemasahu12456 <seemasahu124567@gmail.com>
2026-02-14 18:57:49 +0530

    Update app.js

--- js/app.js
+++ js/app.js
@@ -2,75 +2,205 @@
 // 🎬 VIDEO DATA
 // ===============================
 let videos = [
-  {
-    title: "Cinematic Nature | 4K Ultra HD",
-    category: "Nature",
-    src: "https://vod.wwe.com/vod/2026/,1080,720,540,432,360,288,/dchiraw1707_07_ntwk.m3u8",
-    thumb: "https://www.wwe.com/f/styles/wwe_16_9_l/public/2026/02/20260209_RAW_final.jpg",
+   {
+    title: "",
+    category: "Action & Adventure",
+    src: "https://rumble.com/embed/v73ij4o/?pub=2sznqv",
+    thumb: "https://hugh.cdn.rumble.cloud/video/fwe2/6f/s8/1/G/f/j/Z/GfjZz.qR4e-small-1.jpg",
     channel: "Nature Pro",
-    views: "1.2M views",
+    views: "968 views",
     duration: "5:30",
     date: "2 days ago"
   },
   {
-    title: "Massive intense pleasure brought on by a powerful sex toy",
-    category: "Cars",
-    src: "https://hugh.cdn.rumble.cloud/video/s8/2/B/D/R/x/BDRxm.caa.mp4?u=3&b=0",
-    thumb: "https://hugh.cdn.rumble.cloud/video/s8/6/B/D/R/x/BDRxm.qR4e.jpg",
+    title: "",
+    category: "Action & Adventure",
+    src: "https://rumble.com/embed/v73ijck/?pub=2sznqv",
+    thumb: "https://hugh.cdn.rumble.cloud/video/fwe2/7f/s8/1/8/j/j/Z/8jjZz.qR4e-small-2.jpg",
     channel: "Auto Vision",
     views: "820K views",
     duration: "8:45",
     date: "1 week ago"
   },
   {
-    title: "Future Tech AI | The Next Generation",
-    category: "Tech",
-    src: "videos/4.mp4",
-    thumb: "thumbnails/11.jpg",
+    title: "",
+    category: "Action & Adventure",
+    src: "https://rumble.com/embed/v73ik70/?pub=2sznqv",
+    thumb: "https://hugh.cdn.rumble.cloud/video/fwe2/43/s8/1/e/B/j/Z/eBjZz.qR4e-small-3.jpg",
     channel: "Tech World",
     views: "540K views",
     duration: "12:20",
     date: "3 days ago"
   },
   {
-    title: "Mountain Sunrise | Cinematic Timelapse",
-    category: "Nature",
-    src: "videos/4.mp4",
-    thumb: "thumbnails/3.jpg",
+    title: "",
+    category: "Action & Adventure",
+    src: "https://rumble.com/embed/v73ikty/?pub=2sznqv",
+    thumb: "https://hugh.cdn.rumble.cloud/video/fwe2/46/s8/1/-/N/j/Z/-NjZz.qR4e-small-4.jpg",
     channel: "Nature Pro",
     views: "2.1M views",
     duration: "4:15",
     date: "1 month ago"
   },
   {
-    title: "Hypercar Showcase | 8K Resolution",
-    category: "Cars",
-    src: "videos/4.mp4",
-    thumb: "thumbnails/11.jpg",
+    title: "",
+    category: "Action & Adventure",
+    src: "https://rumble.com/embed/v73il84/?pub=2sznqv",
+    thumb: "https://hugh.cdn.rumble.cloud/video/fwe2/2d/s8/1/8/V/j/Z/8VjZz.qR4e-small-5.jpg",
     channel: "Auto Vision",
     views: "1.5M views",
     duration: "6:40",
     date: "2 weeks ago"
   },
   {
-    title: "Quantum Computing Explained",
-    category: "Tech",
-    src: "videos/4.mp4",
-    thumb: "thumbnails/3.jpg",
+    title: "",
+    category: "Action & Adventure",
+    src: "https://rumble.com/embed/v73imig/?pub=2sznqv",
+    thumb: "https://hugh.cdn.rumble.cloud/video/fww1/3e/s8/6/a/k/k/Z/akkZz.qR4e.jpg",
     channel: "Tech World",
     views: "950K views",
     duration: "15:30",
     date: "4 days ago"
   },
   {
-    title: "Future AI | Next Gen",
-    category: "AI",
-    src: "videos/4.mp4",
-    thumb: "thumbnails/11.jpg",
+    title: "",
+    category: "Action & Adventure",
+    src: "https://rumble.com/embed/v73iqms/?pub=2sznqv",
+    thumb: "https://hugh.cdn.rumble.cloud/video/fww1/c9/s8/1/C/D/l/Z/CDlZz.qR4e-small-7.jpg",
     channel: "AI Hub",
     views: "880K views",
     duration: "10:10",
     date: "5 days ago"
+  },
+    {
+    title: "",
+    category: "Action & Adventure",
+    src: "https://rumble.com/embed/v73jspg/?pub=2sznqv",
+    thumb: "https://hugh.cdn.rumble.cloud/video/fww1/c2/s8/1/C/G/x/Z/CGxZz.qR4e-small-8.jpg",
+    channel: "Nature Pro",
+    views: "968 views",
+    duration: "5:30",
+    date: "2 days ago"
+  },
+  {
+    title: "",
+    category: "Action & Adventure",
+    src: "https://rumble.com/embed/v73jt4s/?pub=2sznqv",
+    thumb: "https://hugh.cdn.rumble.cloud/video/fwe2/25/s8/1/e/P/x/Z/ePxZz.qR4e-small-9.jpg",
+    channel: "Auto Vision",
+    views: "820K views",
+    duration: "8:45",
+    date: "1 week ago"
+  },
+  {
+    title: "",
+    category: "Action & Adventure",
+    src: "https://rumble.com/embed/v73ju5m/?pub=2sznqv",
+    thumb: "https://hugh.cdn.rumble.cloud/video/fww1/20/s8/1/Y/9/x/Z/Y9xZz.qR4e-small-10.jpg",
+    channel: "Tech World",
+    views: "540K views",
+    duration: "12:20",
+    date: "3 days ago"
+  },
+  {
+    title: "",
+    category: "Action & Adventure",
+    src: "https://rumble.com/embed/v73jvqc/?pub=2sznqv",
+    thumb: "https://hugh.cdn.rumble.cloud/video/fwe2/55/s8/1/S/D/y/Z/SDyZz.qR4e-small-11.jpg",
+    channel: "Nature Pro",
+    views: "2.1M views",
+    duration: "4:15",
+    date: "1 month ago"
+  },
+  {
+    title: "",
+    category: "Action & Adventure",
+    src: "https://rumble.com/embed/v73jwzs/?pub=2sznqv",
+    thumb: "https://hugh.cdn.rumble.cloud/video/fwe2/01/s8/1/q/3/y/Z/q3yZz.qR4e-small-12.jpg",
+    channel: "Auto Vision",
+    views: "1.5M views",
+    duration: "6:40",
+    date: "2 weeks ago"
+  },
+  {
+    title: "",
+    category: "Action & Adventure",
+    src: "https://rumble.com/embed/v73jzne/?pub=2sznqv",
+    thumb: "https://hugh.cdn.rumble.cloud/video/fww1/0d/s8/1/c/T/z/Z/cTzZz.qR4e-small-13.jpg",
+    channel: "Tech World",
+    views: "950K views",
+    duration: "15:30",
+    date: "4 days ago"
+  },
+  {
+    title: "",
+    category: "Action & Adventure",
+    src: "https://rumble.com/embed/v73k0fm/?pub=2sznqv",
+    thumb: "https://hugh.cdn.rumble.cloud/video/fwe2/b4/s8/1/6/8/z/Z/68zZz.qR4e-small-14.jpg",
+    channel: "AI Hub",
+    views: "880K views",
+    duration: "10:10",
+    date: "5 days ago"
+  },
+    {
+    title: "",
+    category: "Action & Adventure",
+    src: "https://rumble.com/embed/v73k0t6/?pub=2sznqv",
+    thumb: "https://hugh.cdn.rumble.cloud/video/fwe2/2c/s8/1/I/e/A/Z/IeAZz.qR4e-small-15.jpg",
+    channel: "Nature Pro",
+    views: "968 views",
+    duration: "5:30",
+    date: "2 days ago"
+  },
+  {
+    title: "",
+    category: "Action & Adventure",
+    src: "https://rumble.com/embed/v73k17e/?pub=2sznqv",
+    thumb: "https://hugh.cdn.rumble.cloud/video/fwe2/7a/s8/1/I/m/A/Z/ImAZz.qR4e-small-16.jpg",
+    channel: "Auto Vision",
+    views: "820K views",
+    duration: "8:45",
+    date: "1 week ago"
+  },
+  {
+    title: "",
+    category: "Action & Adventure",
+    src: "https://rumble.com/embed/v73k1pc/?pub=2sznqv",
+    thumb: "https://hugh.cdn.rumble.cloud/video/fwe2/4e/s8/1/O/w/A/Z/OwAZz.qR4e-small-17.jpg",
+    channel: "Tech World",
+    views: "540K views",
+    duration: "12:20",
+    date: "3 days ago"
+  },
+  {
+    title: "",
+    category: "Action & Adventure",
+    src: "https://rumble.com/embed/v73k2i0/?pub=2sznqv",
+    thumb: "https://hugh.cdn.rumble.cloud/video/fwe2/43/s8/1/W/M/A/Z/WMAZz.qR4e-small-18.jpg",
+    channel: "Nature Pro",
+    views: "2.1M views",
+    duration: "4:15",
+    date: "1 month ago"
+  },
+  {
+    title: "",
+    category: "Action & Adventure",
+    src: "https://rumble.com/embed/v73k2ya/?pub=2sznqv",
+    thumb: "https://hugh.cdn.rumble.cloud/video/fwe2/b0/s8/1/6/V/A/Z/6VAZz.qR4e-small-19.jpg",
+    channel: "Auto Vision",
+    views: "1.5M views",
+    duration: "6:40",
+    date: "2 weeks ago"
+  },
+  {
+    title: "",
+    category: "Action & Adventure",
+    src: "https://rumble.com/embed/v73k36m/?pub=2sznqv",
+    thumb: "https://hugh.cdn.rumble.cloud/video/fww1/35/s8/1/M/0/A/Z/M0AZz.qR4e-small-20.jpg",
+    channel: "Tech World",
+    views: "950K views",
+    duration: "15:30",
+    date: "4 days ago"
   }
 ];
 
@@ -240,3 +370,4 @@ shuffleVideos(false);
 
 window.addEventListener("resize", updateCategorySlider);
 setTimeout(updateCategorySlider, 100);
+
